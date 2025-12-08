6baa2814a1f0c4dcc8f030b3ca917e03d388382a
Rahma Elbially <rahmamohammed117@gmail.com>
2025-12-08 15:54:12 +0200

    edit style in statistics section

--- src/app/[locale]/(main)/home/_components/statistics/StatisticsCard.tsx
+++ src/app/[locale]/(main)/home/_components/statistics/StatisticsCard.tsx
@@ -20,7 +20,10 @@ const StatisticsCard = ({
   return (
     <div className="bg-white rounded-2xl border border-[#cee8ff] p-10 text-[#425073]">
       <div
-        className={`icon w-20 h-20 rounded-full flex items-center justify-center bg-[${bgColor}] ${className}`}
+        className={`icon w-20 h-20 rounded-full flex items-center justify-center ${
+          className || ""
+        }`}
+        style={{ backgroundColor: bgColor }}
       >
         <Icon color="white" size={40} />
       </div>
