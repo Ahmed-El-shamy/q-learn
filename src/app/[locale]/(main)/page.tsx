b094590e6eee786ce93dde8cd539bb3d022834ee
Rahma Elbially <rahmamohammed117@gmail.com>
2025-12-08 09:59:51 +0200

    add container component

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -1,8 +1,17 @@
+import Container from "@/_components/common/container/Container";
 import { useTranslations } from "next-intl";
 const HomePage = () => {
   const t = useTranslations("Home");
 
-  return <h1>{t("title")}</h1>;
+  return (
+    <>
+      <section className="h-screen bg-blue-200">
+        <Container className="bg-red-200">
+          <div></div>
+        </Container>
+      </section>
+    </>
+  );
 };
 
 export default HomePage;
