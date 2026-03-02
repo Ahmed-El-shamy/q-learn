fb8a3bab893d32e8448406edd19bb17e3b8a0d2c
Ahmed-El-shamy <ahmedelshamy@qutell.com>
2026-03-02 11:47:22 +0200

    feat: adjusting the UI of the blog details page

--- src/app/[locale]/(main)/blogs/[id]/page.tsx
+++ src/app/[locale]/(main)/blogs/[id]/page.tsx
@@ -4,8 +4,8 @@ import api, { Api } from "@/_lib/api/api";
 import { Blog } from "../_types/blogs.types";
 import { notFound } from "next/navigation";
 import HTMLConvertor from "@/_components/common/HTMLConvertor";
-import TOC from "./_components/TOC";
 import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
 
 export async function generateMetadata({
   params,
@@ -31,60 +31,44 @@ export default async function Page({
   }>;
 }) {
   const { id } = await params;
+  const tCommon = await getTranslations("common");
   const response = await api.get<Blog>(`${Api.routes.site.blogs}/${id}`);
   if (response && response.data) {
     const blog = response.data;
     return (
       <div className="containerr space-between-sections ">
-        <div className="flex flex-col gap-10">
-          <div className="grid grid-cols-1 xl:grid-cols-[1fr_320px] gap-8 items-start">
-            {/* --------------------- Main Content --------------------- */}
-            <article className="border border-[#d1d1d1] overflow-hidden flex flex-col h-full bg-white">
-              <div className="w-full h-72 sm:h-92 md:h-110 xl:h-132 relative overflow-hidden">
-                <Image
-                  src={blog.image}
-                  alt={blog.name}
-                  fill
-                  className="object-cover"
-                />
-              </div>
-
-              <div className="px-5 py-10 md:p-10 flex flex-col gap-8 flex-1">
-                <div className="flex flex-wrap gap-3 md:gap-5">
-                  <div className="flex items-center py-2 sm:py-1 ps-3 sm:ps-1 pe-7 gradient-background rounded-full gap-2 cursor-pointer">
-                    <div className="w-6 sm:w-10 h-6 sm:h-10 rounded-full overflow-hidden">
-                      <Image
-                        src="/images/homepage/business-man1.jpg"
-                        alt="user name"
-                        width={100}
-                        height={100}
-                        className="object-cover"
-                      />
-                    </div>
-                    <span className="text-white text-sm sm:text-base">
-                      {blog.author}
-                    </span>
-                  </div>
-
-                  <div className="flex items-center py-2 sm:py-1 px-7 bg-[#1f2b40] rounded-full">
-                    <span className="text-white text-sm sm:text-base">
-                      {blog.created_at}
-                    </span>
-                  </div>
-                </div>
-
-                <div className="space-y-5">
-                  <h1 className="text-2xl md:text-3xl text-[#3a3636] font-bold">
-                    {blog.name}
-                  </h1>
-                  <HTMLConvertor id="blog_content" html={blog.description} />
-                </div>
-                <BlogSocial />
-              </div>
-            </article>
-            <TOC />
+        <article className="mx-auto max-w-3xl border border-[#d1d1d1] overflow-hidden flex flex-col h-full bg-white">
+          <div className="w-full h-72 sm:h-92 md:h-110 xl:h-132 relative overflow-hidden">
+            <Image
+              src={blog.image}
+              alt={blog.name}
+              fill
+              className="object-cover"
+            />
           </div>
-        </div>
+
+          <div className="px-5 py-10 md:p-10 flex flex-col gap-8 flex-1">
+            <div className="flex flex-wrap gap-3 md:gap-5 items-center text-sm sm:text-base text-[#373737]">
+              <span>{tCommon("publication_date")}</span>
+              <div className="flex items-center py-2 sm:py-1 px-7 bg-[#1f2b40] rounded-full">
+                <span className="text-white text-sm sm:text-base">
+                  {blog.created_at}
+                </span>
+              </div>
+            </div>
+
+            <div className="space-y-5">
+              <h1 className="text-2xl md:text-3xl lg:text-4xl text-[#3a3636] font-bold leading-snug">
+                {blog.name}
+              </h1>
+              <HTMLConvertor id="blog_content" html={blog.description} />
+            </div>
+
+            <div className="pt-6 border-t border-[#e5e5e5] mt-4">
+              <BlogSocial />
+            </div>
+          </div>
+        </article>
       </div>
     );
   }
