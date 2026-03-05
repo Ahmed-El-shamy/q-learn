8be566968cb67f403ed75e50c9b25f7c23aa7be5
Ahmed-El-shamy <ahmedelshamy@qutell.com>
2026-03-05 12:40:31 +0200

    feat: displaying the date at the review car

--- src/app/[locale]/(main)/courses/[id]/_components/ReviewCard.tsx
+++ src/app/[locale]/(main)/courses/[id]/_components/ReviewCard.tsx
@@ -1,3 +1,4 @@
+import { useLocale } from "next-intl";
 import Rate from "../../_components/Rate";
 import Avatar from "@/_components/common/avatar/Avatar";
 
@@ -10,22 +11,35 @@ interface ReviewCardProps {
             id: number;
             name: string;
         };
+        created_at: string;
     };
 }
 
 const ReviewCard = ({ review }: ReviewCardProps) => {
+    const locale = useLocale();
+    const createdAt = new Date(review.created_at).toLocaleDateString(locale === "en" ? "en-GB" : "ar", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+    }); 
+
     return (
         <div className="border border-gray-300 rounded-lg p-3 sm:p-4 md:p-5 bg-white">
             <div className="flex flex-col gap-2 sm:gap-3">
-                <div className="flex items-center gap-2 sm:gap-3">
-                    <Avatar
-                        src="/"
-                        alt={review.user.name}
-                        size={48}
-                        className="w-9 h-9 sm:w-10 sm:h-10 shrink-0"
-                    />
-                    <p className="font-semibold text-xs sm:text-sm md:text-base text-gray-900">
-                        {review.user.name}
+                <div className="flex justify-between items-center">
+                    <div className="flex items-center gap-2 sm:gap-3">
+                        <Avatar
+                            src="/"
+                            alt={review.user.name}
+                            size={48}
+                            className="w-9 h-9 sm:w-10 sm:h-10 shrink-0"
+                        />
+                        <p className="font-semibold text-xs sm:text-sm md:text-base text-gray-900">
+                            {review.user.name}
+                        </p>
+                    </div>
+                    <p className="text-xs sm:text-sm text-gray-500">
+                        {createdAt}
                     </p>
                 </div>
                 <Rate rate={review.rating} size={16} />
